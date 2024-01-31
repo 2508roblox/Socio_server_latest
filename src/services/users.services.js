bd9e00ca365bf67fce8a18d77eac47a4a16f9f93
Huyh Huyhu <2509roblox@gmail.com>
2024-01-31 11:34:05 +0700

    success login register

--- src/services/users.services.js
+++ src/services/users.services.js
@@ -1,23 +1,45 @@
 import User from "../models/schemas/User.schema.js";
 
-// services: database, main logic
 export class UserService {
+  async login(credentials) {
+    const { email, password } = credentials;
+    const user = await User.findOne({ email });
 
-  async login({ user_id, verify }) {
-    const { access_token, refresh_token } = await this.signAccessAndRefreshToken({ user_id, verify })
-    await databaseService.refreshTokens.insertOne(
-      new RefreshToken({ token: refresh_token, user_id: new ObjectId(user_id) })
-    )
-    return { access_token, refresh_token }
+    if (!user) {
+      throw new Error("Invalid email or password");
+    }
+
+    const isPasswordMatch = await user.matchPassword(password);
+
+    if (!isPasswordMatch) {
+      throw new Error("Invalid email or password");
+    }
+
+    return user;
   }
   async register(body) {
-    const { username, password } = body;
-    let isExist = await User.findOne({ username: username })
-    const newUser = new User({ username, password });
+    // {
+    //   "username": "john.doe",
+    //   "email": "john.doe@example.com",
+    //   "password": "password123",
+    //   "firstName": "John",
+    //   "lastName": "Doe",
+    //   "age": 30
+    // }
+    const { username, email, password, firstName, lastName, age } = body;
+    let isExist = await User.findOne({ username, email });
+
+    if (isExist) {
+      throw new Error("User already exists");
+    }
+
+    const newUser = new User({ username, email, password, firstName, lastName, age });
+
     await newUser.save();
     return newUser;
-
   }
 }
-const userService = new UserService()
-export default userService
+
+
+const userService = new UserService();
+export default userService;
